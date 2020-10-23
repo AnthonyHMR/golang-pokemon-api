cd243b45f261c819f630b9d10f463c23475018f1
Anthony <anthonymr2010@hotmail.com>
2020-10-23 01:17:51 -0600

    Cambio en el API
    Se agrega funcion de agregra un pokemon

--- main.go
+++ main.go
@@ -3,20 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"pokemon-api/database"
+
+	"github.com/gorilla/mux"
 )
 
 func getAllPokemons(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(database.PokemonDb)
 }
 
+func newPokemon(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var pokemon database.Pokemon
+	json.Unmarshal(reqBody, &pokemon)
+
+	database.PokemonDb = append(database.PokemonDb, pokemon)
+
+	json.NewEncoder(w).Encode(pokemon)
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
+
+	myRouter.HandleFunc("/pokemons", newPokemon).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
