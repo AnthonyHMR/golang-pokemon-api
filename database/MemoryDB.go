fbebc94f7d780c3c5fda993e0a5ada3006e9ec7b
Anthony <anthonymr2010@hotmail.com>
2020-10-23 19:03:43 -0600

    travis added
    Probando travis

--- database/MemoryDB.go
+++ database/MemoryDB.go
@@ -5,7 +5,7 @@ type Pokemon struct {
 	Name string `json:"Name"`
 	Type string `json:"Type"`
 }
-
+=
 var PokemonDb = []Pokemon{
 	Pokemon{ID: "1", Name: "Pikachu", Type: "Electric"},
 	Pokemon{ID: "2", Name: "Charmeleon", Type: "Fire"},
